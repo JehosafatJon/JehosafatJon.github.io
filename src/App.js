18d64e2f956481bec2f90786d087c2d679001be5
JehosafatJon <122817924+JehosafatJon@users.noreply.github.com>
2023-09-26 13:01:05 -0400

    Updated sidebar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { DefaultHeader } from './components/Default-header';
+import { SidebarTest } from './components/Sidebar-test';
 
 function App() {
   return (
     <div className="App">
       <div className='Horizontal-test'>
-        <div className='Side-bar-test'>
-          <p>test</p>
-        </div>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          JehosafatJon Site Coming Soon!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </div>
+        <SidebarTest />
+        <DefaultHeader />
       </div>
     </div>
   );
