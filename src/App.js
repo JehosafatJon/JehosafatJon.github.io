5f5713fe9863b2046077822c59c9b017124a90f1
Jonathan Hughes <jonathan.hughes@flemingcollege.ca>
2023-09-26 00:38:30 -0400

    New Learning! Flexboxes!
    Looks like i'll need to learn more about html/css before I can continue, but i'm learning!

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-        JehosafatJon Site Coming Soon!
-        </p>
-      </header>
+      <div className='Horizontal-test'>
+        <div className='Side-bar-test'>
+          <p>test</p>
+        </div>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          JehosafatJon Site Coming Soon!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
