4dd8921d7ada6e8f4f1f08d4ed2ce27e608f36e2
JehosafatJon <122817924+JehosafatJon@users.noreply.github.com>
2023-09-26 22:21:58 -0400

    adding component files

---
+++ src/components/Default-header.js
@@ -0,0 +1,20 @@
+import logo from '../logo.svg';
+
+export function DefaultHeader() {
+    return (
+        <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            JehosafatJon Site Coming Soon!
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </div>
+    )
+}
---
+++ src/components/Sidebar-test.js
@@ -0,0 +1,13 @@
+export function SidebarTest() {
+    return (
+        <div className='Side-bar-test'>
+            <h1>Jonathan Hughes</h1>
+            <p>About</p>
+            <p>Educational Experience</p>
+            <p>Professional Experience</p>
+            <p>Extracurricular</p>
+            <p>Interests</p>
+            <p>Socials</p>
+        </div>
+    )
+}
