bb8cf5bd06a566fd60782b02a9c7382a7c30c967
Jonathan Hughes <jonathan.hughes@flemingcollege.ca>
2023-09-29 12:43:14 -0400

    Stuff

--- src/components/Sidebar-test.js
+++ src/components/Sidebar-test.js
@@ -1,13 +1,17 @@
-export function SidebarTest() {
-    return (
-        <div className='Side-bar-test'>
-            <h1>Jonathan Hughes</h1>
-            <p>About</p>
-            <p>Educational Experience</p>
-            <p>Professional Experience</p>
-            <p>Extracurricular</p>
-            <p>Interests</p>
-            <p>Socials</p>
-        </div>
-    )
+import { Component } from "react";
+
+export class SidebarTest extends Component {
+    render() {
+        return (
+            <div className='Side-bar-test'>
+                <h1>Jonathan Hughes</h1>
+                <p>About</p>
+                <p>Educational Experience</p>
+                <p>Professional Experience</p>
+                <p>Extracurricular</p>
+                <p>Interests</p>
+                <p>Socials</p>
+            </div>
+        )
+    }
 }
