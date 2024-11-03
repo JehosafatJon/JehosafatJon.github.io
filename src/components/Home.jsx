0395d27e95697a167d7067a8166e4c3f3fe268dd
Jonathan Hughes <jonathan.hughes@flemingcollege.ca>
2024-11-03 10:20:55 -0500

    removed "e-portfolio"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,11 @@ export function Home() {
         <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h3>
-            Welcome to Jonathan Hughes' E-Portfolio
+            Jonathan Hughes
 
             </h3>
-            <p style={{ fontSize: '18px'}}> This website is a personal project of mine. It is still under active development. I have written every piece of code on this website. I have chosen to build this website completely from scratch to learn the React JavaScript framework for web development.</p>
-            <p style={{ fontSize: '18px'}}>As an example of my software development skills, view page source! For easier viewing, the source code for this website can be found<a href="https://github.com/JehosafatJon/JehosafatJon.github.io" className='animated-link'>here</a>.</p>
+            <p style={{ fontSize: '18px'}}>This website is a personal project of mine. It is still under active development. I have written every piece of code on this website. I have chosen to build this website completely from scratch to learn the React JavaScript framework for web development.</p>
+            <p style={{ fontSize: '18px'}}>Source<a href="https://github.com/JehosafatJon/JehosafatJon.github.io" className='animated-link'>here</a></p>
             
         </div>
     )
